80110ef02965642ae80130c978ce7c7d7c207f71
Ovidiu Constantin <ovidiu@mybox.ro>
2025-12-15 18:47:20 +0200

    Changed color of the itineraty lines, make them lighter gray.

--- src/pdf/components/itinerary.jsx
+++ src/pdf/components/itinerary.jsx
@@ -6,53 +6,53 @@ import { ITINERARY_ITEM, ITINERARY_LINES } from '~/lib/itinerary-utils';
 import PdfConfig from '~/pdf/config';
 
 class Itinerary extends React.PureComponent {
-	styles = StyleSheet.create( {
+	styles = StyleSheet.create({
 		line: {
-			borderBottom: '1 solid #AAA',
+			borderBottom: '1 solid #EEE',
 			fontSize: Math.round((this.props.config?.lineHeight || 20) * 0.6),
 			fontWeight: 'bold',
 			height: this.props.config?.lineHeight || 20,
 			minHeight: this.props.config?.lineHeight || 20,
 			padding: '2 0 0 5',
 		},
-	} );
+	});
 
-	renderItineraryItem = ( { type, value }, index ) => {
-		switch ( type ) {
+	renderItineraryItem = ({ type, value }, index) => {
+		switch (type) {
 			case ITINERARY_ITEM:
-				return this.renderItem( value, index );
+				return this.renderItem(value, index);
 
 			case ITINERARY_LINES:
 			default:
-				return this.renderLines( value );
+				return this.renderLines(value);
 		}
 	};
 
-	renderItem( text, index ) {
+	renderItem(text, index) {
 		return (
-			<Text key={ index } style={ this.styles.line }>
+			<Text key={index} style={this.styles.line}>
 				{text}
 			</Text>
 		);
 	}
 
-	renderLines( count ) {
+	renderLines(count) {
 		const lines = [];
-		for ( let i = 0; i < count; i++ ) {
-			lines.push( <Text key={ i } style={ this.styles.line }></Text> );
+		for (let i = 0; i < count; i++) {
+			lines.push(<Text key={i} style={this.styles.line}></Text>);
 		}
 
 		return lines;
 	}
 
 	render() {
-		return <>{this.props.items.map( this.renderItineraryItem )}</>;
+		return <>{this.props.items.map(this.renderItineraryItem)}</>;
 	}
 }
 
 Itinerary.propTypes = {
 	items: PropTypes.array.isRequired,
-	config: PropTypes.instanceOf( PdfConfig ),
+	config: PropTypes.instanceOf(PdfConfig),
 };
 
 export default Itinerary;
